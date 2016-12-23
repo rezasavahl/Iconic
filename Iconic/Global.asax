d72ba91f597b7a07265f06b2ea1993a33ce1d4a7
rezasavahl <reza@savronit.com>
2016-12-23 10:42:15 +0200

    iconic

---
+++ Iconic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Iconic.MvcApplication" Language="C#" %>
---
+++ Iconic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Iconic.Startup))]
+namespace Iconic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
